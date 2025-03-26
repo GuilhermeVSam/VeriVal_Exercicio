2a02c4fccb5ae7a5d388a8fbc8c7f1ad44280cab
GuilhermeV <guiwojtysiak053@hotmail.com>
2025-03-25 22:07:39 -0300

    Desenvolver os Testes Close #4

---
+++ verival/src/test/java/VerificaIntervaloTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.exerc.VerificaIntervalo;
+
+public class VerificaIntervaloTest {
+
+    @Test
+    @DisplayName("25.01 == Intervalo(25,50]")
+    void checkInterval() {
+        VerificaIntervalo vi = new VerificaIntervalo();
+
+        assertEquals(vi.verificaIntervalo(25.01), "Intervalo (25,50]");
+        assertEquals(vi.verificaIntervalo(25.00), "Intervalo [0,25]");
+        assertEquals(vi.verificaIntervalo(100.00), "Intervalo (75,100]");
+        assertEquals(vi.verificaIntervalo(-25.02), "Fora de Intervalo");
+    }
+}
